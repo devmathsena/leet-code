ce1e4528ad3682fb7dc4cb5999a509c21ea5d2cd
devmathsena <dev.matheussena@gmail.com>
2025-04-21 20:00:49 -0300

    Update 1000.js

--- easy/1000.js
+++ easy/1000.js
@@ -1,5 +1,6 @@
 /*
-Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+Your first program in any programming language is usually "Hello World!". 
+In this first problem all you have to do is print this message on the screen.
 
 Input
 This problem has no input.
